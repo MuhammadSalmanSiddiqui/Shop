158fea93c5799d60b909d131b13d41c61cdc8a22
Muhammad Salman Siddiqui <salmansidd991@gmail.com>
2021-01-15 21:53:53 +0500

    Shop

---
+++ Spice/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Spice.Models;
+using Spice.Utility;
+
+namespace Spice.Areas.Identity.Pages.Account
+{
+    [AllowAnonymous]
+    public class RegisterModel : PageModel
+    {
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly ILogger<RegisterModel> _logger;
+        private readonly IEmailSender _emailSender;
+
+        //You need to add dependent injection for Role Manager
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public RegisterModel(
+            UserManager<IdentityUser> userManager,
+            SignInManager<IdentityUser> signInManager,
+            ILogger<RegisterModel> logger,
+            IEmailSender emailSender,
+            //You need to add dependent injection for Role Manager
+            RoleManager<IdentityRole> roleManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _logger = logger;
+            _emailSender = emailSender;
+            //You need to add dependent injection for Role Manager
+            _roleManager = roleManager;
+        }
+
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+        public string ReturnUrl { get; set; }
+
+        public class InputModel
+        {
+            [Required]
+            [EmailAddress]
+            [Display(Name = "Email")]
+            public string Email { get; set; }
+
+            [Required]
+            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+            [DataType(DataType.Password)]
+            [Display(Name = "Password")]
+            public string Password { get; set; }
+
+            [DataType(DataType.Password)]
+            [Display(Name = "Confirm password")]
+            [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+            public string ConfirmPassword { get; set; }
+
+            [Required]
+            public string Name { get; set; }
+
+            public string StreetAddress { get; set; }
+
+            public string PhoneNumber { get; set; }
+
+            public string City { get; set; }
+            public string State { get; set; }
+
+            public string PostalCode { get; set; }
+        }
+
+        public void OnGet(string returnUrl = null)
+        {
+            ReturnUrl = returnUrl;
+        }
+
+        public async Task<IActionResult> OnPostAsync(string returnUrl = null)
+        {
+            //This is to fetch the role of the user in the register form
+            string role = Request.Form["rdUserRole"].ToString();
+
+            returnUrl = returnUrl ?? Url.Content("~/");
+            if (ModelState.IsValid)
+            {
+                //change IdentityUser to ApplicationUser
+                var user = new ApplicationUser {
+
+                    UserName = Input.Email,
+                    Email = Input.Email,
+                    Name =Input.Name,
+                    City = Input.City,
+                    StreetAddress = Input.StreetAddress,
+                    State= Input.State,
+                    PostalCode=Input.PostalCode,
+                    PhoneNumber = Input.PhoneNumber,
+                };
+                var result = await _userManager.CreateAsync(user, Input.Password);
+                if (result.Succeeded)
+                {
+                    //creating all the roles
+                    if (!await _roleManager.RoleExistsAsync(SD.ManagerUser))
+                    {
+                        await _roleManager.CreateAsync(new IdentityRole(SD.ManagerUser));
+                    }
+                    if (!await _roleManager.RoleExistsAsync(SD.FrontDeskUser))
+                    {
+                        await _roleManager.CreateAsync(new IdentityRole(SD.FrontDeskUser));
+                    }
+                    if (!await _roleManager.RoleExistsAsync(SD.KitchenUser))
+                    {
+                        await _roleManager.CreateAsync(new IdentityRole(SD.KitchenUser));
+                    }
+                    if (!await _roleManager.RoleExistsAsync(SD.CustomerEndUser))
+                    {
+                        await _roleManager.CreateAsync(new IdentityRole(SD.CustomerEndUser));
+                    }
+                    //assigning a role to the user
+
+                    if(role == SD.KitchenUser)
+                    {
+                        await _userManager.AddToRoleAsync(user, SD.KitchenUser);
+                    }
+                    else
+                    {
+                        if (role == SD.FrontDeskUser)
+                        {
+                            await _userManager.AddToRoleAsync(user, SD.KitchenUser);
+                        }
+                    
+                        else
+                        {
+                            if (role == SD.ManagerUser)
+                            {
+                                await _userManager.AddToRoleAsync(user, SD.ManagerUser);
+                            }
+                            else
+                            {
+                                //Only customers will be able to sign immedately after registering
+                                await _userManager.AddToRoleAsync(user, SD.CustomerEndUser);
+                                await _signInManager.SignInAsync(user, isPersistent: false);
+                                //redirecting the customer to the Homeoage after registering
+                                return LocalRedirect(returnUrl);
+                            }
+                        }
+                        Log.Information("{Name} created a new account with email {Email}", user.Name, user.Email);
+                        _logger.LogInformation("User created a new account with password.");
+                    }
+                    //redirecting the manager to the list of users
+                    return RedirectToAction("Index", "User", new { area = "Admin" });
+
+                  
+
+                   
+
+                    //var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+                    //var callbackUrl = Url.Page(
+                    //    "/Account/ConfirmEmail",
+                    //    pageHandler: null,
+                    //    values: new { userId = user.Id, code = code },
+                    //    protocol: Request.Scheme);
+
+                    //await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
+                    //    $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+
+                   
+                  
+                }
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+            }
+
+            // If we got this far, something failed, redisplay form
+            return Page();
+        }
+    }
+}
---
+++ Spice/Models/ApplicationUser.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Spice.Models
+{
+    //creating an Application user model that extends from identity
+    public class ApplicationUser :IdentityUser
+    {
+       
+        public string Name { get; set; }
+        public string StreetAddress { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+
+        public string PostalCode { get; set; }
+    }
+}
